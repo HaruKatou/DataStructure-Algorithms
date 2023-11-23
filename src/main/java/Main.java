6597de36d28efb10327ace85c8447cc0c0a6abf4
HaruKatou <bqtrung1001@gmail.com>
2023-11-23 22:01:52 +0700

    UnionFind

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,15 +3,16 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         List<List<Integer>> edges = new ArrayList<List<Integer>>();
-        edges.add(Arrays.asList(1, 2, 3));
-        edges.add(Arrays.asList(1, 3, 4));
-        edges.add(Arrays.asList(4, 2, 6));
-        edges.add(Arrays.asList(5, 2, 2));
-        edges.add(Arrays.asList(2, 3, 5));
-        edges.add(Arrays.asList(3, 5, 7));
+        edges.add(Arrays.asList(1, 2, 20));
+        edges.add(Arrays.asList(1, 3, 50));
+        edges.add(Arrays.asList(1, 4, 70));
+        edges.add(Arrays.asList(2, 3, 30));
+        edges.add(Arrays.asList(3, 4, 40));
+        edges.add(Arrays.asList(4, 5, 60));
+        edges.add(Arrays.asList(1, 5, 90));
 
 
-        System.out.println(Prim2.prims(5, edges, 1));
+        System.out.println(Prim.prims(5, edges, 2));
     }
 }
 
---
+++ src/main/java/UnionFind.java
@@ -0,0 +1,43 @@
+public class UnionFind {
+    private int[] parent;
+    // 2 operations
+
+    public UnionFind(int size) {
+        parent = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            parent[i] = i;
+        }
+    }
+    public int find(int x) {
+        if (parent[x] != x) {
+            return find(parent[x]);
+        }
+        return x;
+    }
+
+    public void union(int x, int y) {
+        parent[find(y)] = find(x);
+    }
+
+    public boolean isConnected(int x, int y) {
+        return find(x) == find(y);
+    }
+
+    public static void main(String[] args) {
+        UnionFind uf = new UnionFind(5);
+
+        uf.union(0, 1);
+        uf.union(1, 2);
+        uf.union(3, 4);
+
+        System.out.println(uf.isConnected(0, 2)); // true
+        System.out.println(uf.isConnected(1, 3)); // false
+
+        uf.union(2, 3);
+
+        System.out.println(uf.isConnected(0, 2)); // true
+        System.out.println(uf.isConnected(1, 3)); // true
+    }
+
+}
